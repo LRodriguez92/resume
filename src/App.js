8e737b1d5972c67be5af40e133bf1a82ecbff0cc
Leonardo Rodriguez <leo@Leonardos-MacBook-Pro.local>
2021-08-31 13:10:55 -0700

    displaying EducationContainer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import EducationContainer from './components/EducationContainer';
 
 function App() {
   return (
     <div className="App">
-      
+      <EducationContainer />
     </div>
   );
 }
---
+++ src/components/EducationContainer.js
@@ -0,0 +1,12 @@
+import Title from "./Title"
+
+
+const EducationContainer = () => {
+    return (
+        <div>
+            <Title title="Education"/>
+        </div>
+    )
+}
+
+export default EducationContainer
